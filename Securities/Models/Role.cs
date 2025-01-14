21d18781f2affdec0e0353a3f98e984cca22dd83
Nomm <94917416+PhiHung151@users.noreply.github.com>
2025-01-14 17:04:30 +0700

    Add files via upload

---
+++ Securities/Models/Role.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Securities.Models
+{
+    public class RoleModel
+    {
+        public List<Role> RoleDetailLists { get; set; }
+    }
+    public class Role
+    {
+        public int ID { get; set; }
+
+
+        [Required(ErrorMessage = "Enter name, please")]
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+
+        [Required(ErrorMessage = "Choose Status, please")]
+        public int Status { get; set; }
+
+        public DateTime? created_at { get; set; }
+
+        public DateTime? updated_at { get; set; }
+
+        public DateTime? deleted_at { get; set; }
+    }
+}
